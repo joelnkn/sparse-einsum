dfa80fae95600df8a5da0d5e09c93f43859df827
nullplay <jaeyeon@mit.edu>
2025-08-26 14:46:42 -0400

    xx

--- test.py
+++ test.py
@@ -48,6 +48,59 @@ tensors = [
     SparseTensor.random_sparse_tensor(dims, 100, device=torch.device("cpu")) for dims in test_case["tensor_dims"]
 ]
 
+from torch._inductor.pattern_matcher import (
+    Arg,
+    CallFunction,
+    KeywordArg,
+    Match,
+    PatternMatcherPass,
+    register_graph_pattern,
+    register_replacement,
+    fwd_only,
+    joint_fwd_bwd,
+    Ignored
+)
+
+from torch._inductor.fx_passes.post_grad import register_lowering_pattern, pass_patterns
+aten = torch.ops.aten
+
+'''
+        full_7: "b8[1, 100][100, 1]cpu" = torch.ops.aten.full.default([1, 100], True, dtype = torch.bool, layout = torch.strided, device = device(type='cpu'), pin_memory = False)
+
+        bitwise_and_2: "b8[100, 100][100, 1]cpu" = torch.ops.aten.bitwise_and.Tensor(bitwise_and_1, full_7);  bitwise_and_1 = full_7 = None
+
+'''
+#TORCH_LOGS_FORMAT="%(levelname)s: %(message)s" TORCH_LOGS="aot_graphs,output_code,post_grad_graphs" python test.py
+def extra_check_test(match):
+    fill_value = match.kwargs["fill_value"]
+    return fill_value
+
+@register_graph_pattern(
+    CallFunction(
+        aten.bitwise_and.Tensor,
+        KeywordArg("t1"),
+        CallFunction(
+            aten.full.default,
+            KeywordArg("shape"),
+            KeywordArg("fill_value"),
+            dtype=KeywordArg("dtype"),
+            layout=Ignored(),
+            device=KeywordArg("device"),
+            pin_memory=False,
+            #_users=MULTIPLE,
+        ),
+        #_users=MULTIPLE,
+    ),
+    pass_dict=pass_patterns[1],
+    extra_check=extra_check_test,
+)
+def pointless_bwand_replacement(match: Match, shape, fill_value, device, dtype, t1):
+    def repl(x):
+        return x 
+
+    # only replace the output node, not all nodes
+    match.replace_by_example(repl, [t1])
+
 with torch._inductor.utils.fresh_inductor_cache():
     compiled_einsum = torch.compile(sparse_einsum)
     expected = torch.einsum(test_case["equation"], *[t.to_dense() for t in tensors])
